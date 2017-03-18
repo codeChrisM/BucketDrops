a2deda7d65a6855d3b45b9ff5d0f40142013edd6
chris marlowe <chris@codechris.com>
2017-03-18 06:57:26 -0500

    fixed crashing bug, installed realm.io and recycler view

--- app/src/main/java/com/codechris/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/codechris/bucketdrops/ActivityMain.java
@@ -2,12 +2,55 @@ package com.codechris.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.support.v7.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mtoolbar;
+    Button mBtnAdd;
+    RecyclerView mRecyler;
+    private View.OnClickListener mBtnAddListener= new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            showDialogAdd();
+        }
+    };
+
+    private void showDialogAdd() {
+        DialogAdd dialog= new DialogAdd();
+        dialog.show(getSupportFragmentManager(),"Add");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mtoolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mRecyler=(RecyclerView) findViewById(R.id.rv_drops);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
+        setSupportActionBar(mtoolbar);
+        initBackgroundImage();
     }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
+
+
+    }
+
+
 }
