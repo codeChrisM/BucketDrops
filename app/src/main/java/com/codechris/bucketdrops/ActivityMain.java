7073ca5f50efea327a0ee0dfd87ff204def3601e
chris marlowe <chris@codechris.com>
2017-03-25 04:46:54 -0500

    before outdated video

--- app/src/main/java/com/codechris/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/codechris/bucketdrops/ActivityMain.java
@@ -2,6 +2,7 @@ package com.codechris.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,16 @@ import android.widget.Toast;
 import android.support.v7.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.codechris.bucketdrops.adapters.AdapterDrops;
+
+import io.realm.Realm;
 
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar mtoolbar;
     Button mBtnAdd;
     RecyclerView mRecyler;
+    Realm mRealm;
     private View.OnClickListener mBtnAddListener= new View.OnClickListener(){
 
         @Override
@@ -37,6 +42,8 @@ public class ActivityMain extends AppCompatActivity {
         mtoolbar = (Toolbar) findViewById(R.id.toolbar);
         mBtnAdd = (Button) findViewById(R.id.btn_add);
         mRecyler=(RecyclerView) findViewById(R.id.rv_drops);
+
+        mRecyler.setAdapter(new AdapterDrops(this));
         mBtnAdd.setOnClickListener(mBtnAddListener);
         setSupportActionBar(mtoolbar);
         initBackgroundImage();
